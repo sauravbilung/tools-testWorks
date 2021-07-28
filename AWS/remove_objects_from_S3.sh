0a103614622312270131266326e411f321b59869
Saurav Bilung <saurav.bilung@adcolony.com>
2021-07-28 19:02:54 +0530

    Remove objects from S3 using cli

---
+++ AWS/remove_objects_from_S3.sh
@@ -0,0 +1,22 @@
+#!/bin/bash 
+# To run the script "bash scriptName.sh"
+# Or make it executable chmod +x scriptName.sh then ./scriptName.sh
+
+listObjects=$(aws s3api list-objects --bucket sbilung --no-paginate --query Contents[].Key)
+
+deleteObject () {
+  delete=$(aws s3api delete-object --bucket sbilung --key $1)
+  echo "Deletion : "$1" "$delete  
+}
+
+for object in $listObjects
+do
+  if [[ $object =~ .*"GeoIP2-Country-Locations-en.csv".* || $object =~ .*"maxmind-update.txt".* ]]; then
+    deleteObject "$object"
+  fi    
+done 
+
+echo "Done !!!"
+
+
+
